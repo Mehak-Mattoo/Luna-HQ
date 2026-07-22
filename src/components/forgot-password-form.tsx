f05bd53164b4eaeff914e58b70265ea81eb4d24e
Mehak <mehakmattoo7@gmail.com>
2026-07-22 15:39:36 +0530

    FIX: import

--- src/components/forgot-password-form.tsx
+++ src/components/forgot-password-form.tsx
@@ -15,7 +15,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { authRoutes } from "@/app/routes";
+import { authRoutes } from "./helpers/routes";
 
 export function ForgotPasswordForm({
   className,
